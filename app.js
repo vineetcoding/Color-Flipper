97f8a68efc69e4e9f5d4d424f33eaba77ce432fe
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2022-11-17 05:48:08 +0530

    Color Flipper with Color Code

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const buttonClick = document.querySelector('#btn');
 buttonClick.addEventListener('click',function () {
     const colors = 'rgb('+randoms(225)+', '+randoms(225)+', '+randoms(225)+')';
     document.body.style.backgroundColor = colors;
-    document.getElementById('color_code').innerText = colors;
+    // document.getElementById('color_code').innerText = colors;
+    document.getElementById('btn').innerText = colors;
 })
 
 function randoms (number) {
